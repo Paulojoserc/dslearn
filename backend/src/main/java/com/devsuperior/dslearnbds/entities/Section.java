c58dcfecf68745f9ac5ba06c9bcc5f517963ff45
Paulojoserc <pjrc83@gmail.com>
2024-02-14 16:39:16 -0300

    Fix: timestamp without time zone and text

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslearnbds.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,6 +20,7 @@ public class Section implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Integer position;
 	private String imgUri;
