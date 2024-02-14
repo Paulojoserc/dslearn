fc541f705c7cabf17b3b78094b067668922fd240
Paulojoserc <pjrc83@gmail.com>
2024-02-14 15:57:45 -0300

    Enrollment, EnrollmentPK

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Resource.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,6 +25,7 @@ public class Resource  implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Integer position;
 	private String imgUri;
