b9b1bc87d478672ddf99377bce7ce00257b6cceb
Paulojoserc <pjrc83@gmail.com>
2024-02-15 17:56:04 -0300

    Fix: erreção da estrutura da entidade Task

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Task.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Task.java
@@ -26,6 +26,46 @@ public class Task extends Lesson implements Serializable {
 		this.weight = weight;
 		this.dueDate = dueDate;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getQuestionCount() {
+		return questionCount;
+	}
+
+	public void setQuestionCount(Integer questionCount) {
+		this.questionCount = questionCount;
+	}
+
+	public Integer getApprovalCount() {
+		return approvalCount;
+	}
+
+	public void setApprovalCount(Integer approvalCount) {
+		this.approvalCount = approvalCount;
+	}
+
+	public Double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(Double weight) {
+		this.weight = weight;
+	}
+
+	public Instant getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(Instant dueDate) {
+		this.dueDate = dueDate;
+	}
 	
 	
 }
