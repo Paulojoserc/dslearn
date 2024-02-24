f1f6468207f787e819841317a54faf8271f59f8a
Paulojoserc <pjrc83@gmail.com>
2024-02-24 19:36:51 -0300

    Remove user roles on construtor

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -17,6 +17,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "tb_user")
 public class User implements Serializable {
@@ -28,27 +29,25 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name="tb_user_role",
-	joinColumns = @JoinColumn (name = "user_id"),
-	inverseJoinColumns = @JoinColumn(name = "role_id")	)
+	@JoinTable(name = "tb_user_role", 
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
-	
+
 	@OneToMany(mappedBy = "user")
 	private List<Notification> notifications = new ArrayList<>();
 
 	public User() {
 	}
 
-	public User(Long id, String name, String email, String password, Set<Role> roles) {
+	public User(Long id, String name, String email, String password) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.roles = roles;
+
 	}
-	
-	
 
 	public Long getId() {
 		return id;
@@ -89,7 +88,7 @@ public class User implements Serializable {
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
 	}
-	
+
 	public List<Notification> getNotifications() {
 		return notifications;
 	}
